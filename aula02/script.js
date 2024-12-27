dd8ac84e5c504a46e9fccd1c8b085689aaa8af74
Hatoridev <jonasdev21@outlook.com>
2024-12-27 14:44:11 -0300

    Adicionando arquivos da aula02 da linguagem de progamação  JavaScript, Nível Básico

---
+++ aula02/script.js
@@ -0,0 +1,9 @@
+// Seleciona o botão pelo ID
+const btnInfo = document.getElementById("btnInfo");
+
+// Adiciona um evento de clique ao botão
+btnInfo.addEventListener("click", function () {
+  // Exibe um alerta com mais informações
+  alert("Os navegadores modernos possuem diversas responsabilidades, incluindo segurança, renderização de conteúdo e execução de scripts.");
+});
+
